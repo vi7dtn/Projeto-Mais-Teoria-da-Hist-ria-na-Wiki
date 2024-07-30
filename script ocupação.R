26f80f4e2430866ac47f6d70d6c596e94ad5e901
Vicente Detoni <vicente>
2024-07-29 23:08:54 -0300

    Nova atualização do script (limpeza e inserção de mais comentários)

--- script ocupação.R
+++ script ocupação.R
@@ -1,18 +1,48 @@
+### Limpar o environment
 rm(list=ls())
 
-library(dplyr)
+### Carregar os pacotes (lembrar de instalar com install.packages("") a primeira vez que for rodar o código)
+
+library(dplyr) 
 library(readxl)
 library(writexl)
 
-caminho_pt <- ".csv"
-caminho_en <- ".csv"
-caminho_es <- ".csv"
+### Definir o caminho em que os arquivos gerados pelo Wikidata estão em seu PC
+
+# Lembrar de sempre utilizar a barra invertida '/' no caminho do arquivo do seu PC"
+
+caminho_pt <- ".csv" # Arquivo que contém os verbetes na Wikipédia lusófona
+caminho_en <- ".csv" # Arquivo que contém os verbetes na Wikipédia anglo
+caminho_es <- ".csv" # Arquivo que contém os verbetes na Wikipédia hispânica
+
+### Carregar os arquivos
 
 wiki_pt <- read.csv(caminho_pt)
 wiki_en <- read.csv(caminho_en)
 wiki_es <- read.csv(caminho_es)
 
-# Tratamento da coluna "imageLabel"
+### Juntar as três tabelas em uma só com base na coluna "item" (o QID do Wikidata)
+
+result <- full_join(wiki_en, wiki_es, by = "item", suffix = c("_en", "_es")) %>%
+  full_join(wiki_pt, by = "item", suffix = c("", "_pt")) %>%
+  select(article1 = article_en, article2 = article_es, article3 = article, item, imageLabel)
+
+### Selecionar as colunas pertinentes nesse nova tabela
+
+result <- select(result, article3, article1, article2, item, imageLabel)
+
+### Copiar os valores das colunas 'article_en' e 'article_es' para a coluna 'article' se estiverem presentes
+
+# Assim colocamos os verbetes provenientes das outras tabelas na coluna principal, que será a coluna "Artigos em português"
+
+result$article3 <- ifelse(is.na(result$article3), result$article2, result$article3)
+result$article3 <- ifelse(is.na(result$article3), result$article1, result$article3)
+
+### Tratamento da coluna "imageLabel"
+
+# Aqui a ideia é inserir o botão "enviar" na coluna "imagem" quando não houver o link para o Commons
+
+# O código aqui está pouquíssimo otimizado mas por enquanto vou manter assim
 
 transform_imageLabel <- function(imageLabel) {
   if (nzchar(imageLabel)) {  # Verifica se a string não está vazia
@@ -33,21 +63,9 @@ wiki_pt$imageLabel <- ifelse(
   enviar_imagem, 
   "")
 
-# Fazendo o 'match' com base na coluna 'item'
+### Tratamento da coluna article_en e article_es
 
-result <- full_join(wiki_en, wiki_es, by = "item", suffix = c("_en", "_es")) %>%
-  full_join(wiki_pt, by = "item", suffix = c("", "_pt")) %>%
-  select(article1 = article_en, article2 = article_es, article3 = article, item, imageLabel)
-
-result <- select(result, article3, article1, article2, item, imageLabel)
-
-# Copiar os valores das colunas 'article_en' e 'article_es' para a coluna 'article' se estiverem presentes
-
-result$article3 <- ifelse(is.na(result$article3), result$article2, result$article3)
-result$article3 <- ifelse(is.na(result$article3), result$article1, result$article3)
-
-
-# Tratamento da coluna Tradução (quando encontrar um valor, substituir por)
+# Aqui a ideia é inserir o botão clicável "Traduzir" quando encontrar for encontrado um valor
 
 trans_en <- "style=\"text-align: center;\"|{{User:Stanglavine/en|"
 
@@ -101,7 +119,10 @@ result$article2 <- ifelse(
   tratamentoIV(result$article2)
 )
 
-# Tratamento da coluna "Item"
+### Tratamento da coluna "Item"
+
+# Aqui a ideia é inserir o botão clicável "Criar" quando não for encontrado valor nenhum na célula
+# Quando encontrar, formatar o valor para virar um link para o Wikidata
 
 new_item <- "style=\"text-align: center;\"|{{Botão clicável 2|Criar|class=mw-ui-constructive center|url=https://www.wikidata.org/wiki/Special:NewItem}}"
 
@@ -111,13 +132,15 @@ result$item <- ifelse(
   gsub("(Q[0-9]+)", "style=\"text-align: center;\"|[[d:\\1|\\1]]", result$item)
 )
 
-# Renomeando as colunas
+### Renomeando as colunas
+
 names(result) <- c("Artigo em português", 
                    "Artigo em inglês",
                    "Artigo em espanhol", 
                    "Item no Wikidata",
                    "Imagem")
 
-# Exportar
-setwd("D:/reflection/UFRGS/Doutorado/2024/Projeto Mais+/Mais Mulheres/listas a editar/ocupação/economistas")
-write_xlsx(result, "economistasfinal.xlsx")
+### Exportar
+
+setwd("")
+write_xlsx(result, ".xlsx")
