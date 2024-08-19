b4c2ebd499a18539c1f3865ea4b64d8428b2af42
Vicente Detoni <vicente>
2024-08-18 21:44:48 -0300

    Atualização de comentários

--- script finalização listas.R
+++ script finalização listas.R
@@ -1,30 +1,42 @@
+### Limpar ambiente do RStudio
+
 rm(list=ls())
 
-# Carregando os pacotes necessários
+### Carregar os pacotes necessários
 
 library(dplyr)
 library(readxl)
 library(writexl)
 library(stringr)
 
-#Carregar arquivos
+### Definir o caminho dos arquivos
 
 lista <- ".xlsx"
 caminho_destaque <- ".xlsx"
 caminho_orto <- ".xlsx"
 
+### Carregar os arquivos
+
 lista <- read_excel(camiho_lista)
 destaque <- read_excel(caminho_destaque)
 orto <- read_excel(caminho_orto)
 
-# "Incluir coluna "Erros ortográficos"
+### "Incluir coluna "Erros ortográficos"
+
+# Duplicar coluna com outro nome
 
 orto$`Erros ortográficos` <- orto$Artigo
+
+# Renomear colunas
 
 names(orto) <- c("Artigo em português", 
                  "Erros ortográficos")
 
+# Identificar correspondências
+
 lista <- left_join(lista, orto, by = "Artigo em português")
+
+# Formatar coluna para criar botão clicável
 
 tratamentoV <- function(string) {
   gsub("\\[\\[(.*)\\]\\]", "style=\"text-align: center;\"|{{Botão clicável|[[\\1|Corrigir]]}}", string)
@@ -36,16 +48,22 @@ lista$`Erros ortográficos` <- ifelse(
   tratamentoV(lista$`Erros ortográficos`)
 )
 
-# Incluir coluna "Possível destaque"
+### Incluir coluna "Possível destaque"
 
-destaque$Artigo <- gsub("^", "[[", destaque$Artigo)
+# Duplicar coluna com outro nome
 
 destaque$`Possível destaque` <- destaque$Artigo
+
+# Renomear colunas
 
 names(destaque) <- c("Artigo em português", 
                      "Possível destaque")
 
+# Identificar correspondências
+
 lista <- left_join(lista, destaque, by = "Artigo em português")
+
+# Formatar coluna para criar botão clicável
 
 tratamentoVI <- function(string) {
   gsub("\\[\\[(.*)\\]\\]", "style=\"text-align: center;\"|{{Botão clicável|[[\\1|Destacar]]}}", string)
